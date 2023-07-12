839cf5cad6389fcf63deac2736427f3894e9b83c
dev <maxikavia@mail.ru>
2023-07-12 11:04:41 +0300

    testing merge

--- backend/audio/services.py
+++ backend/audio/services.py
@@ -14,9 +14,15 @@ def get_path_upload_track(instance, file):
 
 
 def get_path_upload_cover_track(instance, file):
-    return f'track/cover/user_{instance.user.id}/{file}'
+    return f'trac/cover/user_{instance.user.id}/{file}'
 
 
 def delete_old_file(path_file):
     if os.path.exists(path_file):
         os.remove(path_file)
+
+
+def test(value: str) -> bool:
+    if value is None:
+        return False
+    return True
